45323316493daddad6058a1bb7496d4ef1ddbebe
vtayyab6 <108320281+vtayyab6@users.noreply.github.com>
2023-08-19 13:16:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+const puppeteer = require('puppeteer');
+
+http.createServer(async function (req, res) {
+    console.log(`Just got a request at ${req.url}!`);
+    
+    if (req.url === "/scrape") {
+        const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+        const page = await browser.newPage();
+        await page.goto('https://www.vrlgroup.in/track_consignment.aspx?lrtrack=1&lrno=111');
+        const content = await page.content();
+        await browser.close();
+
+        res.write(content);
+    } else {
+        res.write('Yo!');
+    }
+    
     res.end();
 }).listen(process.env.PORT || 3000);
